9054285e0491759eda5d8a8fff9fe7133c2a6223
Baumel Márton <marcibaumel@gmail.com>
2021-10-29 09:57:57 +0200

    add task to the server

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -3,8 +3,8 @@ import Task from './Task'
 const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
        <>
-        {tasks.map((task, index) => (
-        <Task key={index} task ={task}
+        {tasks.map((task) => (
+        <Task key={task.id} task ={task}
         onDelete = {onDelete}
         onToggle = {onToggle}/>))}
        </>
